87d6e238dcab10c8f049a543f9a582069ebc8add
Mtho <mthontanzi99@gmail.com>
2024-11-10 20:40:51 +0200

    Clean up the files

--- src/router/index.js
+++ src/router/index.js
@@ -5,9 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/', name: 'home', component: HomeView
     },
     {
       path: '/about',
