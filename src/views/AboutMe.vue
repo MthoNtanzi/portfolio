50ade8c7626d89e0bfcd764ef9ef2dc5cf4878f8
Mtho <mthontanzi99@gmail.com>
2024-11-15 11:09:30 +0200

    Create Education Page

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,20 +1,31 @@
 <template>
     <div class="main">
-        <img src="../assets/img/about.jpg" alt="">
-        <div>
+        <img src="../assets/img/about.png" alt="">
+        <div class="about-me">
             <h1>ABOUT ME</h1>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae accusamus totam inventore corrupti iste quas in tempore facere perspiciatis ratione?
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae mollitia magnam a voluptas, reiciendis voluptatem harum. Earum, nostrum ipsam. Iste vitae cumque nobis iusto. Error qui in quam sequi eum.
             </p>
         </div>
     </div>
 </template>
 
 <style scoped>
-    
-    
+
+    .main{
+        display: flex;
+        justify-content: space-around;
+        min-height: 65vh;
+        align-items:flex-end;
+    }
+
     .main img{
-        max-height: 11rem;
+        max-height: 13rem;
         border-radius: 12rem;
+    }
+
+    .about-me{
+        max-width: 30rem;
     }
 </style>
---
+++ src/views/Education.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="main">
+        <div class="education">
+            <h1>EDUCATION</h1>
+            <ul>
+                <li>
+                    <h2>2021</h2>
+                    <h2>IT Varsity</h2>
+                    <p>Diploma in Systems Development</p>
+                </li>
+                <li>
+                    <h2>2021</h2>
+                    <h2>Damelin College</h2>
+                    <p>Diploma in Information Technology</p>
+                </li>
+            </ul>
+        </div>
+        <img src="../assets/img/education.jpg" alt="">
+    </div>
+</template>
+
+<style scoped>
+    .main{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items:flex-end;
+    }
+
+    .main img{
+        max-height: 13rem;
+        border-radius: 12rem;
+    }
+
+    /* h1{
+        font-size: 3rem;
+    }
+
+    h3{
+        font-size: 1.5rem;
+    } */
+
+    ul{
+        margin: 0;
+        padding: 0;
+        list-style-type: none;
+    }
+
+    li{
+        margin: 1.7rem 0;
+    }
+
+    li *{
+        margin: 0.4rem 0;
+    }
+</style>
